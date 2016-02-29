2fcd8417e4f50e2402e89f2f1d5af51fedad21b1
Johan Karl Larsen <johan@itlarsen.net>
2016-02-29 22:51:42 +0100

    generics

---
+++ WinFramework_4_6_1/Generics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<int>();
+            stack.Push(5);
+            stack.Push(10);
+            try
+            {
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine(stack.Pop());
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void Swap<T>(ref T left, ref T right)
+        {
+            T temp = left;
+            left = right;
+            right = temp;
+        }
+    }
+}
---
+++ WinFramework_4_6_1/Generics/Stack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Stack<T>
+    {
+        int position;
+        T[] data = new T[100];
+        public void Push(T obj) => data[position++] = obj;
+        public T Pop() => data[--position];
+    }
+}
