99787f0bc6117ca590a4c65fe2142f4b6b2110dd
Johan Karl Larsen <johan@itlarsen.net>
2016-03-01 15:07:28 +0100

    more generics

--- WinFramework_4_6_1/Generics/Program.cs
+++ WinFramework_4_6_1/Generics/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,6 +24,15 @@ namespace Generics
             {
                 Console.WriteLine(ex.Message);
             }
+            int maxVal = Max(5, 10);
+            string largeS = /*Max("AAA", "BBB");*/ "string is a immutable reference type, not a value type.";
+            Console.WriteLine($"maxVal = {maxVal}  largeS = \"{largeS}\"");
+            BigInteger i1 = 50;
+            BigInteger i2 = BigInteger.Pow(long.MaxValue, byte.MaxValue);
+            
+            //i2 += i2 * i2 * i2;
+            var max = Max(i1, i2);
+            Console.WriteLine("a big biginteger is:\n" + max.ToString("R") +"\n\n" + max);
         }
 
         static void Swap<T>(ref T left, ref T right)
@@ -31,5 +41,8 @@ namespace Generics
             left = right;
             right = temp;
         }
+
+        static T Max<T>(T left, T right) where T : struct, IComparable<T>
+            => left.CompareTo(right) > 0 ? left : right;
     }
 }
