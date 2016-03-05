c4d1d7236683c052aaffefd93debd66703a7775e
Johan Karl Larsen <johan@itlarsen.net>
2016-03-05 20:24:59 +0100

    misc

--- WinFramework_4_6_1/Parsing/Program.cs
+++ WinFramework_4_6_1/Parsing/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,13 @@ namespace Parsing
     {
         static void Main(string[] args)
         {
+            const char beta = '\u03b2';
+            const char bitcoin = '\u20bf';
+
+            Console.WriteLine(CultureInfo.CurrentCulture.DisplayName);
+            Console.WriteLine("CurrentCulture: " + CultureInfo.CurrentCulture.Name);
+            Console.WriteLine("CurrentUICulture: " + CultureInfo.CurrentUICulture.Name);
+
             Console.WriteLine(true.ToString() + " " + false.ToString()); // output capitalize first letter: True False
             bool b;
             Console.WriteLine($"tryparse(1) is {bool.TryParse("1", out b)}");
@@ -20,6 +28,18 @@ namespace Parsing
             Console.WriteLine(1.234D);
             // assume norwegian
             Console.WriteLine(double.Parse("1,234"));
+
+            //CultureInfo no = new CultureInfo("nb-NO");
+            CultureInfo no = CultureInfo.GetCultureInfo("nb-NO");
+            if (CultureInfo.CurrentCulture.LCID == no.LCID)
+            {
+                Console.WriteLine("Norwegian is current culture");
+            }
+            NumberFormatInfo f = (NumberFormatInfo)CultureInfo.CurrentCulture.NumberFormat.Clone();
+            Console.WriteLine(33333333.44.ToString("C",f));
+            f.CurrencySymbol = new string(bitcoin, 5);
+            f.CurrencyGroupSeparator = string.Empty;
+            Console.WriteLine(33333333.44.ToString("C",f));
         }
     }
 }
