034900739937846cee02bb11e0b7ec868a75a715
Johan Karl Larsen <johan@itlarsen.net>
2016-03-10 20:14:26 +0100

    l2s

---
+++ WinFramework_4_6_1/L2Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using DAL.L2S.Nutshell;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace L2Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new NutshellContextDataContext())
+            {
+                var query = from c in db.Customers
+                            where c.Name.Contains("a")
+                            orderby c.Name.Length
+                            select c.Name.ToUpper();
+
+                System.Console.WriteLine(query);
+                System.Console.WriteLine("Result: " + WR(query));
+            }
+        }
+
+        static string WR(IEnumerable<string> seq)
+        {
+            return string.Join(" ", seq);
+        }
+    }
+}
