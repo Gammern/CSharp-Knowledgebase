f96486fe63220ee956cff55476080a638ec352e4
Johan Karl Larsen <johan@itlarsen.net>
2016-03-11 11:22:16 +0100

    linq

--- WinFramework_4_6_1/L2Test/Program.cs
+++ WinFramework_4_6_1/L2Test/Program.cs
@@ -1,14 +1,21 @@
 ﻿using DAL.L2S.Nutshell;
+using System;
 using System.Collections.Generic;
+using System.Data.Linq;
 using System.Linq;
-
+using System.Linq.Expressions;
 
 namespace L2Test
 {
+    // Nutshell lack info on code first, only have L2S and old EF
     class Program
     {
         static void Main(string[] args)
         {
+            Expression<Func<Customer, bool>> first = (c) =>  c.ID == 1 ; // ->LambdaExpression -> Expression
+            Console.WriteLine(first.Body.NodeType);
+            Console.WriteLine(((BinaryExpression)first.Body).Left);
+
             using (var db = new NutshellContextDataContext())
             {
                 var query = from c in db.Customers
@@ -17,13 +24,28 @@ namespace L2Test
                             select c.Name.ToUpper();
 
                 System.Console.WriteLine(query);
-                System.Console.WriteLine("Result: " + WR(query));
+                System.Console.WriteLine("Result: " + TS(query));
+
+                DataLoadOptions opt = new DataLoadOptions();
+                //opt.AssociateWith<Customer>(c => c.Purchases.Where(p => p.Price > 900));
+                opt.LoadWith<Customer>(c => c.Purchases); //force eager loading
+                db.LoadOptions = opt;
+                var q2 = from c in db.Customers
+                         select
+                            from p in c.Purchases
+                            select new { c.Name, p.Price };
+
+                //System.Console.WriteLine(q2);
+
+                foreach (var c1 in q2)
+                {
+                    foreach (var namePrice in c1)
+                        Console.WriteLine(namePrice);
+                    //Console.WriteLine($"{namePrice.Name} +spent {namePrice.Price}");
+                }
             }
         }
 
-        static string WR(IEnumerable<string> seq)
-        {
-            return string.Join(" ", seq);
-        }
+        static string TS<T>(T seq) where T : IEnumerable<string> => string.Join(" ", seq); 
     }
 }
