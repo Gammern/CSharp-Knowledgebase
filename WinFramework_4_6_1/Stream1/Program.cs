d937f2e744644350d96597e9aa06ef28721de4d2
Johan Karl Larsen <johan@itlarsen.net>
2016-03-16 22:34:23 +0100

    stream

--- WinFramework_4_6_1/Stream1/Program.cs
+++ WinFramework_4_6_1/Stream1/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace Stream1
@@ -11,6 +8,30 @@ namespace Stream1
     {
         static void Main(string[] args)
         {
+            ByteDemo();
+            Person p = new Person { Name = "Ola Nordmann", Age = 44, Height = 180.0 };
+            MemoryStream ms = new MemoryStream();
+            p.SaveData(ms);
+            ms.Position = 0;
+            Person p2 = new Person();
+            p2.LoadData(ms);
+            Console.WriteLine($"{p2.Name} {p2.Age} {p2.Height}");
+        }
+
+        static void StremDemo()
+        {
+            var encoding = new UTF8Encoding(
+                encoderShouldEmitUTF8Identifier: false,
+                throwOnInvalidBytes: true);
+
+            Action<string, Encoding> BOM = (s, e) => Console.WriteLine(s + ": " + string.Join(",", e.GetPreamble()));
+            BOM("ASCII", Encoding.ASCII);
+            BOM("UTF7", Encoding.UTF7);
+            BOM("UTF8", Encoding.UTF8);
+            BOM("Unicode", Encoding.Unicode);
+            BOM("UTF32",Encoding.UTF32);
+            BOM("BigEndianUnicode",Encoding.BigEndianUnicode);
+            
             using (var s = new FileStream("test.txt", FileMode.Create))
             {
                 Console.WriteLine(s.CanRead);
@@ -43,6 +64,42 @@ namespace Stream1
                 s.Position = 0;
                 Console.WriteLine(await s.ReadAsync(block,0,block.Length));
             }
+        }
+
+        static void ByteDemo()
+        {
+            using (TextWriter w = File.CreateText("but.txt"))
+                w.WriteLine("but—");
+
+            using (Stream s = File.OpenRead("but.txt"))
+            {
+                for (int b; (b = s.ReadByte()) > 1;)
+                    Console.WriteLine(b + "\t" + (char)b);
+            }
+        }
+
+
+    }
+
+    public class Person
+    {
+        public string Name;
+        public int Age;
+        public double Height;
+        public void SaveData(Stream s)
+        {
+            var w = new BinaryWriter(s);
+            w.Write(Name);
+            w.Write(Age);
+            w.Write(Height);
+            w.Flush();
+        }
+        public void LoadData(Stream s)
+        {
+            var r = new BinaryReader(s);
+            Name = r.ReadString();
+            Age = r.ReadInt32();
+            Height = r.ReadDouble();
         }
     }
 }
---
+++ WinFramework_4_6_1/StreamCompression/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace StreamCompression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CompTest();
+            Console.ReadKey();
+        }
+
+        private async static void CompTest()
+        {
+            const string filename = "compressed.bin";
+            string[] words = "The quick brown fox jumps over the lazy dog".Split();
+            Random rand = new Random();
+
+            using (var s = File.Create(filename))
+            {
+                using (var ds = new DeflateStream(s, CompressionMode.Compress))
+                {
+                    using (var w = new StreamWriter(ds))
+                    {
+                        for (int i = 0; i < 1000; i++)
+                        {
+                            await w.WriteAsync(words[rand.Next(words.Length)] + " ");
+                        }
+                    }
+                }
+            }
+
+            var fi = new FileInfo(filename);
+            var length = fi.Length;
+            Console.WriteLine("Length of compressed file: " + length);
+
+            using (var s = File.OpenRead(filename))
+            {
+                using (var ds = new DeflateStream(s, CompressionMode.Decompress))
+                {
+                    using (var r = new StreamReader(ds))
+                    {
+                        Console.WriteLine(await r.ReadToEndAsync());
+                    }
+                }
+            }
+        }
+    }
+}
