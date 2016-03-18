76a9a57581e8a85e5ff0f020ccde9ead7cbf8400
Johan Karl Larsen <johan@itlarsen.net>
2016-03-18 14:44:05 +0100

    files

---
+++ WinFramework_4_6_1/FilePermissions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.AccessControl;
+using System.IO;
+using System.Security.Principal;
+
+namespace FilePermissions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListPermission();
+        }
+
+        private static void ListPermission()
+        {
+            string filename = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string configfile = filename + ".config";
+            if (!File.Exists(configfile))
+                throw new FileNotFoundException(configfile);
+            FileSecurity sec = File.GetAccessControl(configfile);
+            AuthorizationRuleCollection rules = sec.GetAccessRules(true, true, typeof(NTAccount));
+            foreach (FileSystemAccessRule rule in rules)
+            {
+                Console.WriteLine(rule.AccessControlType);
+                Console.WriteLine(rule.FileSystemRights);
+                Console.WriteLine(rule.IdentityReference.Value);
+                Console.WriteLine();
+            }
+            var sid = new SecurityIdentifier(WellKnownSidType.BuiltinUsersSid, null);
+            string useraccount = sid.Translate(typeof(NTAccount)).ToString();
+            FileSystemAccessRule newRule = new FileSystemAccessRule(useraccount, FileSystemRights.ExecuteFile, AccessControlType.Allow);
+            sec.AddAccessRule(newRule);
+            File.SetAccessControl(configfile, sec);
+        }
+    }
+}
---
+++ WinFramework_4_6_1/Files/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfoTest();
+        }
+
+        private static void FileInfoTest()
+        {
+            FileInfo fi = new FileInfo("c:/bin/FileInfo.txt");
+            Console.WriteLine(fi.Exists);
+            using (TextWriter w = fi.CreateText())
+                w.Write("Some text");
+            Console.WriteLine(fi.Exists);
+            fi.Refresh();
+            Console.WriteLine(fi.Exists);
+
+            Console.WriteLine(fi.Name);
+            Console.WriteLine(fi.FullName);
+            Console.WriteLine(fi.DirectoryName);
+            Console.WriteLine(fi.Directory.Name);
+            Console.WriteLine(fi.Extension);
+            Console.WriteLine(fi.Length);
+
+            fi.Encrypt();
+            //fi.Attributes ^= FileAttributes.Hidden;
+            //fi.IsReadOnly = true;
+            Console.WriteLine(fi.Attributes);
+            Console.WriteLine(fi.CreationTime);
+
+            if (File.Exists("c:/bin/FileInfoX.txt"))
+            {
+                File.Delete("c:/bin/FileInfoX.txt");
+            }
+            fi.MoveTo("c:/bin/FileInfoX.txt");
+
+            DirectoryInfo di = fi.Directory;
+            Console.WriteLine(di.Name);
+            Console.WriteLine(di.FullName);
+            Console.WriteLine(di.Parent.FullName);
+            di.CreateSubdirectory("SubFolder");
+            Console.WriteLine(fi.FullName);
+            //fi.Delete();
+            Console.WriteLine(System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory());
+            Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+        }
+    }
+}
