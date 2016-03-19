2099b51da9b39e7fc7e42ce67ebd99953a974dc6
Johan Karl Larsen <johan@itlarsen.net>
2016-03-19 10:04:36 +0100

    ser

---
+++ WinFramework_4_6_1/DataContract/Person.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace DataContract
+{
+    [DataContract]
+    public class Person
+    {
+        [DataMember]
+        public string Name;
+
+        [DataMember]
+        public int Age;
+
+        public override string ToString()
+        {
+            return $"Name: {Name},  Age: {Age}";
+        }
+    }
+}
---
+++ WinFramework_4_6_1/DataContract/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+// Will the name DataContract clash with the attribute by the same name? NO
+namespace DataContract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataContractTest();
+        }
+
+        private static void DataContractTest()
+        {
+            Person p = new Person { Name = "Stacey", Age = 30 };
+            var ds = new DataContractSerializer(typeof(Person));
+            using (var s = File.Create("person.xml"))
+            {
+                ds.WriteObject(s, p);
+            }
+
+            Person p2;
+            using (var s = File.OpenRead("person.xml"))
+            {
+                p2 = (Person)ds.ReadObject(s);
+            }
+
+            Console.WriteLine(p2);
+        }
+    }
+}
