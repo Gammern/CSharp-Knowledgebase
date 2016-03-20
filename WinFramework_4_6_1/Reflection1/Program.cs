fe1a722aa3150aea0545811533340947111f3795
Johan Karl Larsen <johan@itlarsen.net>
2016-03-20 16:30:31 +0100

    refl

---
+++ WinFramework_4_6_1/Reflection1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Reflection1
+{
+    class Walnut
+    {
+        private bool cracked;
+        public void Crack() { cracked = true; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type t = Assembly.GetExecutingAssembly().GetType("Reflection1.Program");
+            Console.WriteLine(t.AssemblyQualifiedName);
+            Type t2 = Type.GetType(t.AssemblyQualifiedName);
+            Type it = typeof(Int32);
+            Console.WriteLine(it.AssemblyQualifiedName + " " + it.Assembly.CodeBase);
+
+            foreach (Type tt in typeof(System.Environment).GetNestedTypes())
+            {
+                Console.WriteLine(tt.FullName);
+            }
+
+            int i = (int)Activator.CreateInstance(typeof(int));
+
+
+            MemberInfo[] members = typeof(Walnut).GetMembers();
+            foreach (MemberInfo m in members)
+            {
+                Console.WriteLine(m);
+            }
+
+            ToStringInfo();
+        }
+
+        private static void ToStringInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
