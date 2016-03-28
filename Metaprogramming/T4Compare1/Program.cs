2e88359226f700d214e00d3c7fc1c8f0f4656d51
Johan Karl Larsen <johan@itlarsen.net>
2016-03-28 11:52:06 +0200

    comparer

---
+++ Metaprogramming/T4Compare1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4Compare1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object o1 = 3, o2 = 4, ores;
+            string s1 = "Hello"; long l1 = 3;
+            try
+            {
+                ores = greater.of(o1, o2);
+                var res = greater.of(s1, l1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            int ires = greater.of(1, 2);
+            Console.WriteLine(ires);
+        }
+    }
+}
