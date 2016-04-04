bd676a88d5f3125851c0e0d11252a1f4cfcbd5c8
Johan Karl Larsen <johan@itlarsen.net>
2016-04-04 15:25:56 +0200

    ildasm sample

---
+++ Metaprogramming/LazyIntegers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace LazyIntegers
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var lazyInteger = new Lazy<int>(() =>
+            {
+                return new Random().Next();
+            });
+            Console.Out.WriteLine(lazyInteger.Value);
+        }
+    }
+}
