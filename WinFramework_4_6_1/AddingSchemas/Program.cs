14ecdd6bd3d6d47cd3ff23d9425b47536076c61f
Johan Karl Larsen <johan@itlarsen.net>
2016-04-14 22:51:08 +0200

    ublxsd

---
+++ WinFramework_4_6_1/AddingSchemas/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace AddingSchemas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = args[0];
+            if (!Directory.Exists(path))
+            {
+                path = @"C:\Users\johan\Documents\ubl\UBL-2.1\xsd\maindoc";
+            }
+
+            XmlSchemaSet maindocSchemaSet = new XmlSchemaSet(new NameTable());
+            maindocSchemaSet.ValidationEventHandler += MaindocSchemaSet_ValidationEventHandler;
+
+            maindocSchemaSet.AddFilesFromDirectory(path, MaindocSchemaSet_ValidationEventHandler);
+
+            Console.WriteLine("Compiling ...");
+            maindocSchemaSet.Compile();
+
+            using (var writer = File.CreateText("result.txt"))
+            {
+                foreach (XmlSchema schema in maindocSchemaSet.Schemas())
+                {
+                    //Console.WriteLine(schema.TargetNamespace);
+                    writer.WriteLine(schema.TargetNamespace);
+                }
+            }
+
+        }
+
+        private static void MaindocSchemaSet_ValidationEventHandler(object sender, ValidationEventArgs arg)
+        {
+            Console.WriteLine($"{arg.Severity}: {arg.Message}");
+        }
+    }
+}
---
+++ WinFramework_4_6_1/AddingSchemas/XmlSchemaSetExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace AddingSchemas
+{
+    public static class XmlSchemaSetExtensions
+    {
+        public static void AddFilesFromDirectory(this XmlSchemaSet @this, string path, ValidationEventHandler validationEventHandler)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            XmlReaderSettings settings = new XmlReaderSettings
+            {
+                ValidationType = ValidationType.Schema,
+                ValidationFlags = XmlSchemaValidationFlags.ProcessInlineSchema,
+                DtdProcessing = DtdProcessing.Parse,
+                NameTable = @this.NameTable
+            };
+
+            // For some reason unknow, need to add this file first
+            using (var reader = XmlReader.Create(@"C:\Users\johan\Documents\ubl\UBL-2.1\xsd\common\UBL-xmldsig-core-schema-2.1.xsd", settings))
+            {
+                Console.WriteLine($"Processing: {Path.GetFileName(reader.BaseURI)} (PRE-LOAD)");
+                XmlSchema schema = XmlSchema.Read(reader, validationEventHandler);
+                @this.Add(schema);
+                //@this.Compile();
+            }
+
+            foreach (var xsdFile in dirInfo.GetFiles("*.xsd"))
+            {
+                using (var reader = XmlReader.Create(xsdFile.FullName, settings))
+                {
+                    Console.WriteLine($"Processing: {xsdFile.Name}");
+                    // Message = "For security reasons DTD is prohibited in this XML document. 
+                    // To enable DTD processing set the DtdProcessing property on XmlReaderSettings to Parse and pass the settings into XmlReader.Create method."
+                    XmlSchema schema = XmlSchema.Read(reader, validationEventHandler);
+                    @this.Add(schema);
+                }
+                //Console.WriteLine(xsdFile.Name);
+            }
+        }
+    }
+}
