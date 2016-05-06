74a6f49b3fe4d92f519e16e59346ce0769fcda67
Johan Karl Larsen <johan@itlarsen.net>
2016-05-06 08:46:27 +0200

    namespaces in unit

---
+++ WinFramework_4_6_1/CodeCompileUnitTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCompileUnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CodeCompileUnit ccu = new CodeCompileUnit();
+            CodeNamespace globalNs = new CodeNamespace();
+            ccu.Namespaces.Add(globalNs);
+            globalNs.Imports.Add(new CodeNamespaceImport( "Bar"));
+            
+            CodeNamespace barNs = new CodeNamespace("Bar");
+            CodeNamespace fooNs = new CodeNamespace("Foo");
+            fooNs.Imports.Add(new CodeNamespaceImport("Bar"));
+            ccu.Namespaces.Add(barNs);
+            ccu.Namespaces.Add(fooNs);
+            
+            new CSharpCodeProvider().GenerateCodeFromCompileUnit(ccu, Console.Out, null);
+            Console.ReadLine();
+        }
+    }
+}
