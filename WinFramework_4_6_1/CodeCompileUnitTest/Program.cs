c89aa74d2c5a0d095603e7b5944d59767952c83c
Johan Karl Larsen <johan@itlarsen.net>
2016-05-06 23:38:56 +0200

    identifier problem

--- WinFramework_4_6_1/CodeCompileUnitTest/Program.cs
+++ WinFramework_4_6_1/CodeCompileUnitTest/Program.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.CSharp;
 using System;
 using System.CodeDom;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
+using System.Xml.Serialization;
 
 namespace CodeCompileUnitTest
 {
@@ -15,16 +19,32 @@ namespace CodeCompileUnitTest
             CodeCompileUnit ccu = new CodeCompileUnit();
             CodeNamespace globalNs = new CodeNamespace();
             ccu.Namespaces.Add(globalNs);
-            globalNs.Imports.Add(new CodeNamespaceImport( "Bar"));
-            
-            CodeNamespace barNs = new CodeNamespace("Bar");
-            CodeNamespace fooNs = new CodeNamespace("Foo");
-            fooNs.Imports.Add(new CodeNamespaceImport("Bar"));
-            ccu.Namespaces.Add(barNs);
-            ccu.Namespaces.Add(fooNs);
-            
-            new CSharpCodeProvider().GenerateCodeFromCompileUnit(ccu, Console.Out, null);
-            Console.ReadLine();
+            var schemaSet = new XmlSchemaSet();
+            var schema = schemaSet.Add(null, "myderived.xsd");
+            schemaSet.Compile();
+            var schemas = new XmlSchemas();
+            foreach (XmlSchema s in schemaSet.Schemas())
+            {
+                schemas.Add(s);
+            }
+            schemas.Compile(ValidationEventHandler, true);
+
+            XmlSchemaImporter importer = new XmlSchemaImporter(schemas);
+            XmlCodeExporter exporter = new XmlCodeExporter(globalNs, null, CodeGenerationOptions.GenerateOrder);
+            foreach (XmlSchemaElement element in schema.Elements.Values)
+            {
+                XmlTypeMapping mapping = importer.ImportTypeMapping(element.QualifiedName);
+                exporter.ExportTypeMapping(mapping);
+            }
+            using (StreamWriter writer = File.CreateText("statystype.cs"))
+            {
+                new CSharpCodeProvider().GenerateCodeFromCompileUnit(ccu, writer, new CodeGeneratorOptions { BracingStyle = "C" });
+            }
+        }
+
+        static void ValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            Console.WriteLine($"{e.Severity} {e.Message}");
         }
     }
 }
