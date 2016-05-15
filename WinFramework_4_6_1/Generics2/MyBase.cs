ee6cd900e913c9bdf26b02a60614af4d49d9a70e
Johan Karl Larsen <johan@itlarsen.net>
2016-05-15 15:49:36 +0200

    more generics

---
+++ WinFramework_4_6_1/Generics2/MyBase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Generics2
+{
+    //public class MyBase<T> where T:MyBase<T> // or class from
+    [System.Xml.Serialization.XmlTypeAttribute("MyBase", Namespace = "urn:oasis:names:specification:ubl:schema:xsd:MyBase")]
+    public abstract class MyBase<T> where T:MyBase<T>
+    {
+        // T is not used, need it to make static field unique amongst inherited classes
+        static string ns;
+
+        static MyBase()
+        {
+            Console.WriteLine($"static ctor ");
+        }
+
+        public MyBase()
+        {
+         //   this.Namespace = (this.GetType().GetCustomAttributes(typeof(XmlTypeAttribute), false).FirstOrDefault() as XmlTypeAttribute).Namespace;
+        }
+
+        public string Namespace
+        {
+            get
+            {
+                if(string.IsNullOrEmpty(ns))
+                {
+                    ns = (this.GetType().GetCustomAttributes(typeof(XmlTypeAttribute), false).FirstOrDefault() as XmlTypeAttribute).Namespace;
+                    Console.WriteLine($"ns set to {ns}");
+                }
+                return ns;
+            }
+        }
+    }
+
+    [System.Xml.Serialization.XmlTypeAttribute("Class1", Namespace = "urn:oasis:names:specification:ubl:schema:xsd:Class1")]
+    class MyClass1 : MyBase<MyClass1>
+    {
+
+    }
+
+    [System.Xml.Serialization.XmlTypeAttribute("Class1", Namespace = "urn:oasis:names:specification:ubl:schema:xsd:Class2")]
+    class MyClass2 : MyBase<MyClass2>
+    {
+        
+    }
+
+}
---
+++ WinFramework_4_6_1/Generics2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass1 c1 = new MyClass1();
+            Console.WriteLine(c1.Namespace);
+            MyClass2 c2 = new MyClass2();
+            MyClass2 c21 = new MyClass2();
+            MyClass2 c22 = new MyClass2();
+            MyClass2 c23 = new MyClass2();
+            Console.WriteLine(c2.Namespace);
+        }
+    }
+}
