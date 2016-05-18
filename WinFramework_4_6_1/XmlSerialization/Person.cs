75283430acf5d731e7ed349d9a4684ac7e4f0d5b
Johan Karl Larsen <johan@itlarsen.net>
2016-05-18 21:41:24 +0200

    XmlInclude only works inside envelope kind of. bummer

--- WinFramework_4_6_1/XmlSerialization/Person.cs
+++ WinFramework_4_6_1/XmlSerialization/Person.cs
@@ -3,19 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Serialization;
 
 namespace XmlSerialization
 {
+    [Serializable]
+    [XmlType("Envelope", Namespace = "http://mynamespace/test/2016/envelope")]
+    public class EnvelopeType
+    {
+        public PersonType Person { get; set; }
+    }
     // props/fields must be public in order to be serialized with XmlSerializer
     // XmlSerializer writes elements in the order that they’re defined in the class.
-
     [Serializable]
-    [XmlType(Namespace = "http://mynamespace/test/2016/3")]
-    [XmlRoot("Person", Namespace = "http://mynamespace/test/2016/3", IsNullable = false)]
+    [XmlIncludeAttribute(typeof(StudentType))]
+    [XmlIncludeAttribute(typeof(TeacherType))]
+    [XmlType("Person",Namespace = "http://mynamespace/test/2016/person")]
+    [XmlRoot("Person", Namespace = "http://mynamespace/test/2016/person", IsNullable = false)]
     public abstract class PersonType
     {
-        [XmlAttribute]
+        [XmlAttribute(Namespace = "http://mynamespace/test/2016/person")]
         public int ID;
         [XmlElement("FirstName")]
         public string Name;
@@ -25,16 +33,23 @@ namespace XmlSerialization
         public DateTime DOB;
         public byte[] KeyData;
         public override string ToString() => $"Name: {Name}, Age: {Age}";
+        [XmlNamespaceDeclarations]
+        public XmlSerializerNamespaces XmlNs = new XmlSerializerNamespaces(new[]
+        {
+            new XmlQualifiedName("p", "http://mynamespace/test/2016/person"),
+            new XmlQualifiedName("s", "http://mynamespace/test/2016/student"),
+            new XmlQualifiedName("t", "http://mynamespace/test/2016/teacher")
+        });
     }
 
     [Serializable]
-    [XmlRoot("Student", Namespace = "http://mynamespace/test/2016/3", IsNullable = false)]
-    [XmlType(Namespace = "http://mynamespace/test/2016/3")]
-    public class Student : PersonType { };
+    //[XmlRoot("Student", Namespace = "http://mynamespace/test/2016/sudent", IsNullable = false)]
+    [XmlType("Student", Namespace = "http://mynamespace/test/2016/student")]
+    public class StudentType : PersonType { };
     [Serializable]
-    [XmlType(Namespace = "http://mynamespace/test/2016/3")]
-    [XmlRoot("Teacher", Namespace = "http://mynamespace/test/2016/3", IsNullable = false)]
-    public class Teacher : PersonType { };
+    [XmlType("Teacher", Namespace = "http://mynamespace/test/2016/teacher")]
+    //[XmlRoot("Teacher", Namespace = "http://mynamespace/test/2016/teacher", IsNullable = false)]
+    public class TeacherType : PersonType { };
 
     public class Address
     {
--- WinFramework_4_6_1/XmlSerialization/Program.cs
+++ WinFramework_4_6_1/XmlSerialization/Program.cs
@@ -4,41 +4,58 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Serialization;
 
 namespace XmlSerialization
 {
     class Program
     {
-        static Type[] subtypes = new Type[] {typeof(Student), typeof(Teacher) };
+        static Type[] subtypes = new Type[] {typeof(StudentType), typeof(TeacherType) };
         static void Main(string[] args)
         {
-            PersonType t = new Teacher { Name = "Stacey", Age = 30, ID = 2, KeyData = new byte[] { 1, 2, 3, 4, 5 } };
-            PersonType s = new Student { Name = "Stacey", Age = 30, ID = 2, KeyData = new byte[] { 1, 2, 3, 4, 5 }, HomeAddress = new Address { Street="Mystreet 5", PostCode="1234" } };
+            PersonType t = new TeacherType { Name = "Teacher", Age = 30, ID = 2, KeyData = new byte[] { 1, 2, 3, 4, 5 } };
+            PersonType s = new StudentType { Name = "Student", Age = 30, ID = 2, KeyData = new byte[] { 1, 2, 3, 4, 5 }, HomeAddress = new Address { Street="Mystreet 5", PostCode="1234" } };
             SerializePerson(t, "teacher.xml");
             SerializePerson(s, "student.xml");
 
+
             PersonType p2 = DeserializePerson("teacher.xml");
+            Console.WriteLine(p2);
+            p2 = DeserializePerson("student.xml");
             Console.WriteLine(p2);
         }
 
         private static PersonType DeserializePerson(string path)
         {
+            EnvelopeType e;
             PersonType p2 = null;
             using (var s = File.OpenRead(path))
             {
-                var xs = new XmlSerializer(typeof(PersonType),subtypes);
-                object o = xs.Deserialize(s);
+                var xs = new XmlSerializer(typeof(EnvelopeType));//,subtypes);
+                e = (EnvelopeType)xs.Deserialize(s);
+                p2 = e.Person;
             }
             return p2;
         }
+        static public XmlSerializerNamespaces XmlNs = new XmlSerializerNamespaces(new[]
+        {
+            new XmlQualifiedName("e", "http://mynamespace/test/2016/envelope"),
+            new XmlQualifiedName("p", "http://mynamespace/test/2016/person"),
+            new XmlQualifiedName("s", "http://mynamespace/test/2016/student"),
+            new XmlQualifiedName("t", "http://mynamespace/test/2016/teacher")
+        });
 
         private static void SerializePerson(PersonType p, string path)
         {
-            var xs = new XmlSerializer(p.GetType());
+            EnvelopeType e = new EnvelopeType
+            {
+                Person = p
+            };
+            var xs = new XmlSerializer(typeof(EnvelopeType));
             using (var s = File.Create(path))
             {
-                xs.Serialize(s, p);
+                xs.Serialize(s, e);//, XmlNs);
             }
         }
     }
